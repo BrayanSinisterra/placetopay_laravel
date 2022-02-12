70f206a617b03fe3bc60cc1d1ccf7c2d14796537
Brayan Sinisterra <brayansinisterra96@gmail.com>
2022-02-12 15:00:42 -0500

    creacion de migraciones

---
+++ database/migrations/2022_02_12_193154_create_orders.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name', 80);
+            $table->string('customer_email', 120);
+            $table->string('customer_mobile', 40);
+            $table->string('reference', 80);
+            $table->text('product_description', 120);
+            $table->string('currency', 10);
+            $table->bigInteger('total');
+            $table->integer('requestId')->nullable();;
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
